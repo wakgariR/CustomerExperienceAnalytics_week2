c1d8aea3bcec7b316373e3b8980c208bf6f45dbe
wakgariR <wakgarirobel@gmail.com>
2025-11-30 11:05:30 +0300

    clean data and save to csv

--- scripts/config.py
+++ scripts/config.py
@@ -29,12 +29,7 @@ SCRAPING_CONFIG = {
 
 # File Paths
 DATA_PATHS = {
-    'raw': 'data/raw',
-    'processed': 'data/processed',
-    'raw_reviews': 'data/raw/reviews_raw.csv',
-    'processed_reviews': 'data/processed/reviews_processed.csv',
-    'sentiment_results': 'data/processed/reviews_with_sentiment.csv',
-    'final_results': 'data/processed/reviews_final.csv'
+    'processed': '../data/processed', 
 }
 
 
